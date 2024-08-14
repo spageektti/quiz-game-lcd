a8f62306c795410aa472529aaeaa8bb7dc3ba035
spageektti <git@spageektti.cc>
2024-08-14 23:04:33 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -23,6 +23,47 @@ void setup() {
     pixel.show();
 }
 
-void loop() {
-    // TODO
+String questions[] = {"2+2=?", "3+5=?", "7-4=?"};
+String answers[][4] = {{"3", "4", "5", "6"}, {"7", "8", "9", "10"}, {"2", "3", "4", "5"}};
+int correctAnswers[] = {1, 1, 2};
+int currentQuestion = 0;
+int selectedAnswer = 0;
+
+void displayQuestion() {
+    lcd.clear();
+    lcd.print(questions[currentQuestion]);
+    lcd.setCursor(0, 1);
+    lcd.print("A: " + answers[currentQuestion][0]);
 }
+
+void displayAnswers() {
+    lcd.setCursor(0, 1);
+    lcd.print("A: " + answers[currentQuestion][selectedAnswer]);
+}
+
+void loop() {
+    displayQuestion();
+
+    int sliderValue = analogRead(SLIDER_PIN);
+    selectedAnswer = map(sliderValue, 0, 1023, 0, 3);
+    displayAnswers();
+
+    if (digitalRead(BUTTON_PIN) == LOW) {
+        if (selectedAnswer == correctAnswers[currentQuestion]) {
+            tone(BUZZER_PIN, 1000, 200);
+            pixel.setPixelColor(0, pixel.Color(0, 255, 0));
+        } else {
+            tone(BUZZER_PIN, 200, 200);
+            pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+        }
+        pixel.show();
+        delay(1000);
+        pixel.setPixelColor(0, pixel.Color(0, 0, 0));
+        pixel.show();
+
+        currentQuestion = (currentQuestion + 1) % 3;
+    }
+
+    delay(100);
+}
+
